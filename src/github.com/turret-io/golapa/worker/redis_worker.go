18fd0050a7dfbbdaf88fcc2405d9b63e8b469337
Tim Henrich <tim.henrich@gmail.com>
2014-08-23 09:33:45 -0400

    Namespace bug

--- src/github.com/turret-io/golapa/worker/redis_worker.go
+++ src/github.com/turret-io/golapa/worker/redis_worker.go
@@ -29,7 +29,7 @@ func main() {
                 if err != nil {
                         panic(err)
                 }
-                mailer := &StandardEmailer{nil}
+                mailer := &golapa.StandardEmailer{nil}
 				mailer.Send(msg)
         }
 
