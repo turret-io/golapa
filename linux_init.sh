082a9e25c9bebd792a4a77e4420bbb2c0493308e
Tim Henrich <tim.henrich@gmail.com>
2014-08-28 08:29:21 -0400

    Updating linux_init.sh

--- linux_init.sh
+++ linux_init.sh
@@ -13,11 +13,11 @@ VOLUME_PATH=$CWD
 GOPROJ_PATH=$CWD/src/github.com/turret-io/golapa
 SRC_DIR=golapa
 
-if [ ! -d $PROJECT_PATH ]; then
-    mkdir $PROJECT_PATH
+if [ ! -d $GOPROJ_PATH ]; then
+    mkdir -p $GOPROJ_PATH
 fi
 
-rsync -av $SRC_DIR $GOPROJ_PATH --exclude .git
+rsync -av $SRC_DIR/* $GOPROJ_PATH --exclude .git
 docker run -v $VOLUME_PATH:/data --name golapa-build-volume busybox true
 # Create .shared_volume symbolic link so we match other init scripts
 ln -s $CWD .shared_volume
