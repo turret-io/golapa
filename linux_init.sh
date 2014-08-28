d2132ed7c065c0062fc61676a97c8271d1b83ece
Tim Henrich <tim.henrich@gmail.com>
2014-08-27 22:06:32 -0400

    Adding symbolic link to linux_init.sh Adding mercurial, git, bzr to go-runtime image

--- linux_init.sh
+++ linux_init.sh
@@ -16,4 +16,6 @@ if [ ! -d $VOLUME_PATH ]; then
 fi
 
 docker run -v $VOLUME_PATH:/data --name golapa-build-volume busybox true
+# Create .shared_volume symbolic link so we match other init scripts
+ln -s $CWD .shared_volume
 docker build -t turretio/go-runtime docker/go-runtime
